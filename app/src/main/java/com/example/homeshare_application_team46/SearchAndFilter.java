5cc7c95d69f5f4fe74c6a7a0bd7e92a124a44aa1
Amanda Horton <amhorton@usc.edu>
2022-10-23 14:24:43 -0700

    Creating Activities

---
+++ app/src/main/java/com/example/homeshare_application_team46/SearchAndFilter.java
@@ -0,0 +1,14 @@
+package com.example.homeshare_application_team46;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchAndFilter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_and_filter);
+    }
+}
