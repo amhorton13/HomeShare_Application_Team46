b80f3171c78a8d5047130c76e1b74e0341e51479
Amanda Horton <amhorton@usc.edu>
2022-10-23 14:10:16 -0700

    Initial commit

---
+++ app/src/main/java/com/example/homeshare_application_team46/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.homeshare_application_team46;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
