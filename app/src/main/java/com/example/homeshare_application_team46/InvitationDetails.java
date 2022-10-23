f0e6c458103efd6ca3006b9d53826f2328ce207a
Amanda Horton <amhorton@usc.edu>
2022-10-23 14:30:52 -0700

    Creating Activities

--- app/src/main/java/com/example/homeshare_application_team46/InvitationDetails.java
+++ app/src/main/java/com/example/homeshare_application_team46/InvitationDetails.java
@@ -9,6 +9,6 @@ public class InvitationDetails extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_invitation_details);
+        //setContentView(R.layout.activity_invitation_details);
     }
 }
--- app/src/main/java/com/example/homeshare_application_team46/SearchAndFilter.java
+++ app/src/main/java/com/example/homeshare_application_team46/SearchAndFilter.java
@@ -9,6 +9,6 @@ public class SearchAndFilter extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search_and_filter);
+        //setContentView(R.layout.activity_search_and_filter);
     }
 }
