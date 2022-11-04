ee0ad955432c5a569c01084c794c085cc3fc0417
Juan Valderrama <jvalderr@usc.edu>
2022-11-04 12:13:18 -0700

    Register Page, Login Page done with firebase auth

--- app/src/main/java/com/example/homeshare_application_team46/LoginPage.java
+++ app/src/main/java/com/example/homeshare_application_team46/LoginPage.java
@@ -1,24 +1,143 @@
 package com.example.homeshare_application_team46;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class LoginPage  extends AppCompatActivity {
+
+
+    private EditText mEmail,mPassword;
+    private FirebaseDatabase database;
+    private DatabaseReference myRef;
+    private Button btnSubmit;
+    private static final String TAG = "AddToDatabase";
+    private Boolean isNameDup = false;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) { // Second activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
 
+        mEmail = (EditText) findViewById(R.id.etEmail);
+        mPassword = (EditText) findViewById(R.id.etPassword);
+        btnSubmit = (Button) findViewById(R.id.btnSubmit);
+
+
+
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference().child("Users");
+        String key = database.getReference("Users").push().getKey();
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = mEmail.getText().toString();
+                String password = mPassword.getText().toString();
+                signIn(email, password);
+            }
+        });
     }
     public void openRegister(View view){
         Intent intent = new Intent(view.getContext(), RegisterPage.class);
         startActivity(intent);
     }
+
+
+    /**
+     * customizable toast
+     * @param message
+     */
+    private void toastMessage(String message){
+        Toast.makeText(this,message,Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            currentUser.reload();
+        }
+    }
+    private void signIn(String email, String password) {
+        Log.d(TAG, "signIn:" + email);
+        if (!validateForm()) {
+            return;
+        }
+
+//        showProgressBar();
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            System.out.println(user.getUid());
+//                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+//                            Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
+//                                    Toast.LENGTH_SHORT).show();
+//                            updateUI(null);
+                        }
+                    }
+                });
+    }
+
+    private boolean validateForm() {
+        boolean valid = true;
+
+        String email = mEmail.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            mEmail.setError("Required.");
+            valid = false;
+        } else {
+            mEmail.setError(null);
+        }
+
+        String password = mPassword.getText().toString();
+        if (TextUtils.isEmpty(password)) {
+            mPassword.setError("Required.");
+            valid = false;
+        } else {
+            mPassword.setError(null);
+        }
+
+        return valid;
+    }
+
 
 
 
--- app/src/main/java/com/example/homeshare_application_team46/RegisterPage.java
+++ app/src/main/java/com/example/homeshare_application_team46/RegisterPage.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -17,6 +21,7 @@ import android.widget.Toast;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.ValueEventListener;
+import com.google.android.gms.tasks.Task;
 
 import java.util.ArrayList;
 
@@ -30,6 +35,7 @@ public class RegisterPage extends AppCompatActivity {
     private String message;
     private static final String TAG = "AddToDatabase";
     public Boolean isNameDup = false;
+    private FirebaseAuth mAuth;
 
 
 
@@ -42,8 +48,6 @@ public class RegisterPage extends AppCompatActivity {
         database = FirebaseDatabase.getInstance();
         myRef = database.getReference().child("Users");
         String key = database.getReference("Users").push().getKey();
-//        myRef = database.getReference().child("message");
-        //        myRef.setValue("inside of register");
 
         btnSubmit = (Button) findViewById(R.id.btnSubmit);
         mUserName = (EditText) findViewById(R.id.etName);
@@ -52,7 +56,8 @@ public class RegisterPage extends AppCompatActivity {
         mAge = (EditText) findViewById(R.id.etAge);
         mBio = (EditText) findViewById(R.id.etBio);
 
-
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
 
 
         btnSubmit.setOnClickListener(new View.OnClickListener() {
@@ -64,83 +69,85 @@ public class RegisterPage extends AppCompatActivity {
                 String userName = mUserName.getText().toString();
                 String email = mEmail.getText().toString();
                 String password = mPassword.getText().toString();
+                String bio = mBio.getText().toString();
                 Integer age = 0;
                 try {
                     age = Integer.parseInt(mAge.getText().toString());
-                } catch(NumberFormatException nfe) {
+                } catch (NumberFormatException nfe) {
                     System.out.println("Could not parse " + nfe);
                 }
-                String bio = mBio.getText().toString();
+                isNameDup = false;
 
-                // Boolean if duplicate userName
-
-
-
-                // Reading info from database and checking if there are duplicate userName
-                Integer finalAge = age;
-                myRef.addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot snapshot) {
-                        ArrayList<Invitation> users = new ArrayList<>();
-                        System.out.println("VALUE FROM DATA" + snapshot.getChildren());
-                        for (DataSnapshot x: snapshot.getChildren()) {
-                            System.out.println("FIRST iterating:  " + x.getValue());
-                            for (DataSnapshot t: x.getChildren()) {
-                                System.out.println("SECOND iterating:  " + t.getValue());
-                                if (t.getKey().equals("username")) {
-                                    System.out.println("  SAME KEY USER   " );
-                                    if (t.getValue().equals(userName)) {
-                                        System.out.println("duplicate ALERT");
-                                        isNameDup = true;
-                                    }
-                                }
-                            }
-                        }
-
-                        if (isNameDup == false) {
-                            addToDatabase(userName, email, password, finalAge, bio, key);
-                        }
-                        else {
-                            toastMessage("UserName already in use dummy, try again");
-                        }
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) {
-
-                        Log.d("Reading ing", "Failed to read in value");
-                    }
-                });
+                createUser(email, password, userName, age, bio);
 
 
-
-                Log.d(TAG, "onClick: Attempting to submit to database: \n" +
-                        "name: " + userName + "\n" +
-                        "email: " + email + "\n" +
-                        "password: " + password + "\n" +
-                        "age: " + age + "\n" +
-                        "bio: " + bio + "\n"
-                );
-
+//                // Reading info from database and checking if there are duplicate userName
+//                Integer finalAge = age;
+//                myRef.addValueEventListener(new ValueEventListener() {
+//                    @Override
+//                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                        for (DataSnapshot x: snapshot.getChildren()) {
+//                            for (DataSnapshot t: x.getChildren()) {
+//                                if (t.getKey().equals("username")) {
+//                                    if (t.getValue().equals(userName)) {
+//                                        System.out.println("duplicate ALERT");
+//                                        isNameDup = true;
+//                                    }
+//                                }
+//                            }
+//                        }
+//
+//
+////                        addToDatabase(isNameDup, userName, email, password, finalAge, bio, key);
+//                            //Todo Create an intent into the main activity
+//                            // Todo: pass in the user data into the next intent
+//
+//
+//                    }
+//
+//                    @Override
+//                    public void onCancelled(@NonNull DatabaseError error) {
+//
+//                        Log.d("Reading ing", "Failed to read in value");
+//                    }
+//                });
+//
+//
+//
+//                Log.d(TAG, "onClick: Attempting to submit to database: \n" +
+//                        "name: " + userName + "\n" +
+//                        "email: " + email + "\n" +
+//                        "password: " + password + "\n" +
+//                        "age: " + age + "\n" +
+//                        "bio: " + bio + "\n"
+//                );
+//
+//            }
+//        });
             }
         });
 
-
     }
-    public void addToDatabase(String userName, String email, String password, Integer age, String bio, String key) {
+    public void addToDatabase(String userID, String userName, String email, String password, Integer age, String bio) {
         //handle the exception if the EditText fields are null
         // TODO: remove userID from user, make sure username is unique
-        if(!userName.equals("") && !email.equals("") && !password.equals("") && !(age==null) && !bio.equals("")){
+        if (!isNameDup && !userName.equals("") && !email.equals("") && !password.equals("") && !(age==null) && !bio.equals("")){
             User userInformation = new User(email,userName,password, age, bio);
-            myRef.child(key).setValue(userInformation);
+            myRef.child(userID).setValue(userInformation);
             toastMessage("Info has been saved!");
             mUserName.setText("");
             mEmail.setText("");
             mPassword.setText("");
             mAge.setText("");
             mBio.setText("");
-        }else{
-            toastMessage("Fill out all the fields loser");
+        }else {
+            if (isNameDup) {
+                toastMessage("UserName already in use dummy, try again");
+            }
+            else {
+                toastMessage("Fill out all the fields loser");
+            }
+
         }
     }
 
@@ -151,6 +158,27 @@ public class RegisterPage extends AppCompatActivity {
     private void toastMessage(String message){
         Toast.makeText(this,message,Toast.LENGTH_SHORT).show();
     }
+    public void createUser(String email, String password, String userName, Integer age, String bio) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            addToDatabase(user.getUid(), userName, email, password, age, bio );
+
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+
+                        }
+                    }
+                });
+    }
+
 
 
 }
