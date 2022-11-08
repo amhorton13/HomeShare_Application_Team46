8a63765964af7357762712e751a253c69b089658
Juan Valderrama <jvalderr@usc.edu>
2022-11-08 02:40:29 -0800

    can respond to invitations but not your own hehe

--- app/src/main/java/com/example/homeshare_application_team46/InvitationDetails.java
+++ app/src/main/java/com/example/homeshare_application_team46/InvitationDetails.java
@@ -13,6 +13,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -28,14 +29,18 @@ import java.util.HashMap;
 public class InvitationDetails extends AppCompatActivity {
 
     String invID;
+    String userID;
+    Boolean ownProfile = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_invitation_details);
+
         /* TODO: set the correct values for the invitation details frontend */
         Intent intent = getIntent();
         invID = intent.getStringExtra("inviteID");
+
         /* TODO: Query DB using invID to get the other information and set the value  */
         System.out.println("INV ID INSIDE OF INV DETAILS:  " + invID);
         TextView propNameDetails = (TextView) findViewById(R.id.etName);
@@ -44,28 +49,15 @@ public class InvitationDetails extends AppCompatActivity {
         TextView numbdrmDetails = (TextView) findViewById(R.id.etNumBeds);
         TextView numbathDetails = (TextView) findViewById(R.id.etNumBaths);
         TextView dateDetails = (TextView) findViewById(R.id.etDate);
+        TextView btnSubmit = (TextView) findViewById(R.id.respondButton);
+        userID = FirebaseAuth.getInstance().getCurrentUser().getUid();
 
-        displayInvitedetails(invID, propNameDetails, numbdrmDetails, numbathDetails, rentpriceDetails, addDetails, dateDetails);
-    }
-
-    public void responseHandler(View view){
-        String userID = FirebaseAuth.getInstance().getCurrentUser().getUid();
-        // Firebase instance
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference().child("Invitations").child(invID);
-        // New map to add
-        HashMap<String, Object> result = new HashMap<>();
-        result.put(userID, false);
-        myRef.child("Responses").setValue(result);
-        // Call intent to profile
-        Intent intent = new Intent(this, ProfilePage.class);
-        startActivity(intent);
+        displayInvitedetails(invID, propNameDetails, numbdrmDetails, numbathDetails, rentpriceDetails, addDetails, dateDetails, btnSubmit);
     }
 
 
 
-
-    public void displayInvitedetails(String invID, TextView propNameDetails, TextView numbdrmDetails, TextView numbathDetails, TextView rentpriceDetails, TextView addDetails, TextView dateDetails){
+    public void displayInvitedetails(String invID, TextView propNameDetails, TextView numbdrmDetails, TextView numbathDetails, TextView rentpriceDetails, TextView addDetails, TextView dateDetails, TextView btnSubmit){
         DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();;
         //Invitation temp = new Invitation();
         mDatabase.child("Invitations").child(invID).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
@@ -75,15 +67,50 @@ public class InvitationDetails extends AppCompatActivity {
                     Log.e("firebase", "Error getting data", task.getException());
                 }
                 else {
-                    propNameDetails.setText((String)task.getResult().child("propName").getValue());
+                    propNameDetails.setText((String) task.getResult().child("propName").getValue());
                     rentpriceDetails.setText(Integer.toString(Math.toIntExact((Long) task.getResult().child("price").getValue())));
-                    addDetails.setText((String)task.getResult().child("address").getValue());
+                    addDetails.setText((String) task.getResult().child("address").getValue());
                     numbdrmDetails.setText(Integer.toString(Math.toIntExact((Long) task.getResult().child("num_bdrm").getValue())));
                     numbathDetails.setText(Integer.toString(Math.toIntExact((Long) task.getResult().child("num_bath").getValue())));
-                    dateDetails.setText((String)task.getResult().child("date_and_time").getValue());
+                    dateDetails.setText((String) task.getResult().child("date_and_time").getValue());
+
+
+                    btnSubmit.setOnClickListener(new View.OnClickListener() {
+
+                        @Override
+                        public void onClick(View view) {
+
+                            System.out.println("INSIDE OF BUTTON ONCLICK TASK VAL:  " + task.getResult().child("poster").getValue());
+                            System.out.println("INSIDE OF BUTTON ONCLICK USEERID:  " + userID);
+                            System.out.println("INSIDE OF BUTTON ONCLICK USEERID:  " + task.getResult().child("poster").getValue() == userID);
+                            // Firebase instance
+                            if (task.getResult().child("poster").getValue().equals(userID)) {
+                                toastMessage("This is YOUR invite, please go bajameswhack");
+                            }
+                            else {
+                                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                                DatabaseReference myRef = database.getReference().child("Invitations").child(invID);
+                                // New map to add
+                                HashMap<String, Object> result = new HashMap<>();
+                                result.put(userID, false);
+                                myRef.child("Responses").setValue(result);
+                                // Call intent to profile
+                                Intent intent = new Intent(InvitationDetails.this, ProfilePage.class);
+                                startActivity(intent);
+                            }
+
+                        }
+                    });
                 }
             }
         });
 
+    }
+    /**
+     * customizable toast
+     * @param message
+     */
+    private void toastMessage(String message){
+        Toast.makeText(this,message,Toast.LENGTH_SHORT).show();
     }
 }
