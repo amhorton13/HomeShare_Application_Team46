5392d7fe92fc35d900fecffbf874fc1456d6356d
JameswHarris <jameswha@usc.edu>
2022-11-08 01:29:28 -0800

    Profile calls DB

--- app/src/main/java/com/example/homeshare_application_team46/ProfilePage.java
+++ app/src/main/java/com/example/homeshare_application_team46/ProfilePage.java
@@ -30,88 +30,25 @@ public class ProfilePage extends AppCompatActivity {
 
     private FirebaseDatabase database;
     private DatabaseReference myRef;
-    private User userProf;
+    private User userProf = new User();
+    private String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_page);
 
-        //TODO: Query DB using username from intent, fill in information to be used by profile page
-        String userID = FirebaseAuth.getInstance().getCurrentUser().getUid();
-        System.out.println("ID: " + userID);
-        userProf = User.queryUser(userID);
-
+        userID = FirebaseAuth.getInstance().getCurrentUser().getUid();
 
         //TODO: Get User from intent
+
         String username = "Loading";
         TextView nameView = (TextView) findViewById(R.id.username);
         nameView.setText(username);
 
-        //Test Data
-        User testUser = new User("JamesHarris@usc.edu", "Jameswah", "i like cs", 12, "idk");
-        User testUser2 = new User("jj@usc.edu", "jjVal", "peepeepoopoo", 43, "milfs");
-
-        testUser.addInvitation(new Invitation("testUser", "minn", "1", "popo", 1500, "USC", 2, 2));
-        testUser.addInvitation(new Invitation("testUser", "jkdnfjks", "2", "date", 2000, "Orchard", 1, 1));
-        testUser.addResponse(new Invitation("testUser2", "jks","3", "date", 2300, "The Moon", 4, 2));
-
-
         LinearLayout layout = (LinearLayout) findViewById(R.id.scrollLayout);
         LayoutInflater li = LayoutInflater.from(this);
-        queryUser(userID, layout, li);
-//        LinearLayout layout = (LinearLayout) findViewById(R.id.scrollLayout);
-//        LayoutInflater li = LayoutInflater.from(this);
-//        Intent intent = getIntent();
-//        if(intent.getExtras() != null && intent.getStringExtra("profileSetting").equals("responses")) {
-//            for(Invitation inv : testUser.getResponses()) {
-//                ConstraintLayout item = (ConstraintLayout) li.inflate(R.layout.feed_item, layout, false);
-//                //set price
-//                TextView price = (TextView) item.getChildAt(0);
-//                String pText = "$" + Integer.toString(inv.getPrice());
-//                price.setText(pText);
-//                //set bedBath
-//                TextView bedBath = (TextView) item.getChildAt(2);
-//                int numBed = inv.getNum_bdrm();
-//                int numBath = inv.getNum_bath();
-//                String bbText = Integer.toString(numBed) + " Bed " + Integer.toString(numBath) + " Bath";
-//                bedBath.setText(bbText);
-//                //set details
-//                TextView details = (TextView) item.getChildAt(3);
-//                String location = inv.getAddress();
-//                String poster = "inv.getPoster().getUsername()";
-//                String dText = location + " by " + poster;
-//                details.setText(dText);
-//                layout.addView(item);
-//            }
-//            Button resButton = (Button) findViewById(R.id.responses);
-//            resButton.setBackgroundColor(Color.GRAY);
-//        }
-//        else{
-//            for(Invitation inv : testUser.getInvitations()) {
-//                ConstraintLayout item = (ConstraintLayout) li.inflate(R.layout.feed_item, layout, false);
-//                //set price
-//                TextView price = (TextView) item.getChildAt(0);
-//                String pText = "$" + Integer.toString(inv.getPrice());
-//                price.setText(pText);
-//                //set bedBath
-//                TextView bedBath = (TextView) item.getChildAt(2);
-//                int numBed = inv.getNum_bdrm();
-//                int numBath = inv.getNum_bath();
-//                String bbText = Integer.toString(numBed) + " Bed " + Integer.toString(numBath) + " Bath";
-//                bedBath.setText(bbText);
-//
-//                //set details
-//                TextView details = (TextView) item.getChildAt(3);
-//                String location = inv.getAddress();
-//                String dText = location;
-//                details.setText(dText);
-//                layout.addView(item);
-//            }
-//            Button invButton = (Button) findViewById(R.id.activeInv);
-//            invButton.setBackgroundColor(Color.GRAY);
-//        }
-
+        queryUser(layout, li);
     }
 
     public void showInvites(View view){
@@ -131,14 +68,13 @@ public class ProfilePage extends AppCompatActivity {
     //TODO: figure out getting inviteID from view and adding to intent
     public void openInvitation(View view){
         Intent intent = new Intent(this, InvitationDetails.class);
-        intent.putExtra("inviteID", 1);
+        intent.putExtra("inviteID", (String) view.getTag());
         startActivity(intent);
     }
 
 
-    public void queryUser(String userID, LinearLayout layout, LayoutInflater li){
+    public void queryUser(LinearLayout layout, LayoutInflater li){
         DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();;
-        User temp = new User();
         mDatabase.child("Users").child(userID).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
             @Override
             public void onComplete(@NonNull Task<DataSnapshot> task) {
@@ -146,28 +82,45 @@ public class ProfilePage extends AppCompatActivity {
                     Log.e("firebase", "Error getting data", task.getException());
                 }
                 else {
-                    temp.setEmail((String) task.getResult().child("email").getValue());
-                    temp.setPassword((String) task.getResult().child("password").getValue());
-                    temp.setBiography((String) task.getResult().child("biography").getValue());
-                    temp.setUsername((String) task.getResult().child("username").getValue());
-                    System.out.println("ONCOMPLETE" + temp.getUsername());
+                    userProf.setEmail((String) task.getResult().child("email").getValue());
+                    userProf.setPassword((String) task.getResult().child("password").getValue());
+                    userProf.setBiography((String) task.getResult().child("biography").getValue());
+                    userProf.setUsername((String) task.getResult().child("username").getValue());
 
                     TextView nameView = (TextView) findViewById(R.id.username);
-                    nameView.setText(temp.getUsername());
+                    nameView.setText(userProf.getUsername());
 
                     TextView bioView = (TextView) findViewById(R.id.bio);
-                    bioView.setText(temp.getBiography());
+                    bioView.setText(userProf.getBiography());
+                }
+            }
+        });
+        mDatabase.child("Invitations").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (!task.isSuccessful()) {
+                    Log.e("firebase", "Error getting data", task.getException());
+                }
+                else {
+                    for(DataSnapshot t : task.getResult().getChildren()){
+                        String poster = (String) t.child("poster").getValue();
+                        String propName = (String) t.child("propName").getValue();
+                        String date = (String) t.child("date_and_time").getValue();
+                        String invID = (String) t.child("invitation_id").getValue();
+                        String address = (String) t.child("address").getValue();
+                        int numBath = ((Long) t.child("num_bath").getValue()).intValue();
+                        int numBed = ((Long) t.child("num_bdrm").getValue()).intValue();;
+                        int price = ((Long) t.child("price").getValue()).intValue();;
 
-                    Invitation testInv1 = new Invitation("testUser", "minn", "1", "popo", 1500, "USC", 2, 2);
-                    Invitation testInv2 = new Invitation("testUser", "jkdnfjks", "2", "date", 2000, "Orchard", 1, 1);
-                    Invitation testInv3 = new Invitation("testUser2", "jks","3", "date", 2300, "The Moon", 4, 2);
-                    temp.addInvitation(testInv1);
-                    temp.addInvitation(testInv2);
-                    temp.addResponse(testInv1);
+
+                        if(userID.equals(poster)){
+                            userProf.addInvitation(new Invitation(poster, propName, invID, date, price, address, numBed, numBath));
+                        }
+                    }
 
                     Intent intent = getIntent();
                     if(intent.getStringExtra("profileSetting") != null && intent.getStringExtra("profileSetting").equals("responses")) {
-                        for(Invitation inv : temp.getResponses()) {
+                        for(Invitation inv : userProf.getResponses()) {
                             ConstraintLayout item = (ConstraintLayout) li.inflate(R.layout.feed_item, layout, false);
                             //set price
                             TextView price = (TextView) item.getChildAt(0);
@@ -191,7 +144,7 @@ public class ProfilePage extends AppCompatActivity {
                         resButton.setBackgroundColor(Color.GRAY);
                     }
                     else{
-                        for(Invitation inv : temp.getInvitations()) {
+                        for(Invitation inv : userProf.getInvitations()) {
                             ConstraintLayout item = (ConstraintLayout) li.inflate(R.layout.feed_item, layout, false);
                             //set price
                             TextView price = (TextView) item.getChildAt(0);
@@ -217,11 +170,6 @@ public class ProfilePage extends AppCompatActivity {
 
 
                 }
-//                try {
-//                    Thread.sleep(3000);
-//                } catch (InterruptedException e) {
-//                    e.printStackTrace();
-//                }
             }
         });
 
